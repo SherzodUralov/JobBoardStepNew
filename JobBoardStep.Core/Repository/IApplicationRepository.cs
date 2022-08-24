0da72a5bc28b91a398a43c8233e352fc22309e8f
jurabek98.jalolov <jurabek98.jalolov@outlook.com>
2022-08-24 13:41:58 +0300

    asdf

---
+++ JobBoardStep.Core/Repository/IApplicationRepository.cs
@@ -0,0 +1,16 @@
+﻿using JobBoardStep.Core.Models;
+using JobBoardStep.Core.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobBoardStep.Core.Repository
+{
+    public interface IApplicationRepository
+    {
+        IList<ApplicationListViewModel> GetAll();
+        void Add(AppCreateVMmin appCreate);
+    }
+}
